ae242f87a3eda92b3828e03ef96fa573e4de7bf4
梁永赞 <lansebidiao@gmail.com>
2020-01-08 16:54:15 +0800

    init project

---
+++ TRUNetworking/Classes/TRUNetworking.h
@@ -0,0 +1,17 @@
+//
+//  TRUNetworking.h
+//  TRUNetworking_Example
+//
+//  Created by azzan on 2019/12/12.
+//  Copyright © 2019 梁永赞. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TRUNetworking : NSObject
+- (void)printWorld:(NSString * )world;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TRUNetworking/Classes/TRUNetworking.m
@@ -0,0 +1,15 @@
+//
+//  TRUNetworking.m
+//  TRUNetworking_Example
+//
+//  Created by azzan on 2019/12/12.
+//  Copyright © 2019 梁永赞. All rights reserved.
+//
+
+#import "TRUNetworking.h"
+
+@implementation TRUNetworking
+- (void)printWorld:(NSString * )world{
+    NSLog(@"%@", world);
+}
+@end
